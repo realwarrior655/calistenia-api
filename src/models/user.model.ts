552ff2fcd0bfec71ef35b1e382b43da8a205f128
ivo_bazan <ivanbazan12xd@gmail.com>
2025-12-13 21:04:41 -0300

    borré las credenciales en el .txt y agregué la interface user y su modelo

---
+++ src/models/user.model.ts
@@ -0,0 +1,47 @@
+import { Schema, model } from "mongoose";
+import { IUser } from "../interfaces/IUsers";
+import e from "express";
+
+const UserSchema = new Schema<IUser>({
+  name: {
+    type: String,
+    required: [true, "El nombre es obligatorio"],
+  },
+  email: {
+    type: String,
+    required: [true, "El correo es obligatorio"],
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: [true, "La contraseña es obligatoria"],
+  },
+
+  img: {
+    type: String,
+  },
+  rol: {
+    type: String,
+    required: true,
+    enum: ["ADMIN_ROLE", "USER_ROLE"],
+  },
+  estado: {
+    type: Boolean,
+    default: true,
+  },
+  google: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+UserSchema.methods.toJSON = function () {
+  const { __v, password, _id, ...user } = this.toObject();
+  user.uid = _id;
+  return user;
+};
+
+const UserModel = model<IUser>("User", UserSchema);
+
+export default UserModel;
